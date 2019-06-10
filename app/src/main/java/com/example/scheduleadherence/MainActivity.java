4f94d8e3653ed11be5adf890dd4e20db03a396af
jerrywdeveloper <50670128+jerrywdeveloper@users.noreply.github.com>
2019-06-10 17:14:09 -0300

    File Creation

---
+++ app/src/main/java/com/example/scheduleadherence/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.scheduleadherence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //constants
+    final
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
